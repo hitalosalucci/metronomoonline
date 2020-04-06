bdb5e944df3c0c9f0a4fc68b66612a88cde84e69
Hitalo Salucci <hitalo.salucci@hotmail.com>
2020-04-06 18:56:09 -0300

    adição de anuncios de tecaldo e imagens do radapé e laterais

--- js/animacoes.js
+++ js/animacoes.js
@@ -19,11 +19,21 @@ function random_lateral(){
     return Math.floor(Math.random() * 4);
 }
 
+function random_lateral2(){
+    return Math.floor(Math.random() * 6);
+}
+
 function random_rodape(){
     return Math.floor(Math.random() * 3);
 }
 
+function random_rodape2(){
+    return Math.floor(Math.random() * 4);
+}
+
 window.onload = carrega_anuncio_lateral();
+window.onload = carrega_anuncio_lateral2();
+
 function carrega_anuncio_lateral(){
     const img_lateral = document.getElementById('img_anuncio_lateral');
 
@@ -42,7 +52,30 @@ function carrega_anuncio_lateral(){
     }
 }
 
+function carrega_anuncio_lateral2(){
+    const img_lateral = document.getElementById('img_anuncio_lateral_teclado');
+
+    random_lateral2();
+
+    if (random_lateral2() == 0){
+        img_lateral.src = 'anuncios/laterais/anuncio1.webp';
+    } else if (random_lateral2() == 1){
+        img_lateral.src = 'anuncios/laterais/anuncio2.webp';
+    } else if (random_lateral2() == 2){
+        img_lateral.src = 'anuncios/laterais/anuncio3.webp';
+    } else if (random_lateral2() == 3){
+        img_lateral.src = 'anuncios/laterais/anuncio4.webp';
+    } else if (random_lateral2() == 4){
+        img_lateral.src = 'anuncios/laterais/anuncio5.webp';
+    } else if (random_lateral2() == 5){
+        img_lateral.src = 'anuncios/laterais/anuncio6.webp';
+    } else {
+        img_lateral.src = 'anuncios/laterais/anuncio1.webp';
+    }
+}
+
 window.onload = carrega_anuncio_rodape();
+window.onload = carrega_anuncio_rodape2();
 function carrega_anuncio_rodape(){
     const img_rodape = document.getElementById('img_anuncio_rodape');
 
@@ -56,6 +89,24 @@ function carrega_anuncio_rodape(){
         img_rodape.src = 'anuncios/rodape/bannergi3.webp';
     } else {
         img_rodape.src = 'anuncios/rodape/bannergi1.webp';
+    }
+}
+
+function carrega_anuncio_rodape2(){
+    const img_rodape = document.getElementById('img_anuncio_rodape_teclado');
+
+    random_rodape2();
+
+    if (random_rodape2() == 0){
+        img_rodape.src = 'anuncios/rodape/anuncio1.webp';
+    } else if (random_rodape2() == 1){
+        img_rodape.src = 'anuncios/rodape/anuncio2.webp';
+    } else if (random_rodape2() == 2){
+        img_rodape.src = 'anuncios/rodape/anuncio3.webp';
+    } else if (random_rodape2() == 3){
+        img_rodape.src = 'anuncios/rodape/anuncio4.webp';
+    } else {
+        img_rodape.src = 'anuncios/rodape/anuncio1.webp';
     }
 }
 
